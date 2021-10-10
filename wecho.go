eea301f8b1f200d20687ffa29319f1970e6efa42
Carlos Ortiz <chrono.dark@gmail.com>
2021-10-10 11:58:53 -0600

    v0.1

--- wecho.go
+++ wecho.go
@@ -19,12 +19,29 @@ func echo(w http.ResponseWriter, req *http.Request){
 	}
 	fmt.Fprintln(w, string(body))
 }
+
+func notFound(w http.ResponseWriter, req *http.Request){
+	w.WriteHeader(http.StatusNotFound)
+	echo(w, req)
+}
+func serverError(w http.ResponseWriter, req *http.Request){
+	w.WriteHeader(http.StatusInternalServerError)
+	echo(w,req)
+}
+
+func invalidRequest(w http.ResponseWriter, req *http.Request){
+	w.WriteHeader(http.StatusBadRequest)
+	echo(w,req)
+}
 func main(){
 	var port int
 	flag.IntVar(&port, "port", 8666, "Port number")
 	flag.Parse()
 	log.Printf("Starting Echo server in port %d \n", port)
 	http.HandleFunc("/", echo)
+	http.HandleFunc("/404", notFound)
+	http.HandleFunc("/500", serverError)
+	http.HandleFunc("/400", invalidRequest)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port),nil)
 	if err !=nil {
 		log.Fatalln(err)
